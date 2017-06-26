a952e3fd1ea84f2f11e2bafad999148704750a02
Leibelhecht <leibelhecht@gmail.com>
2017-06-26 14:23:14 -0500

    todo

--- client/controllers/loginController.js
+++ client/controllers/loginController.js
@@ -1,28 +1,32 @@
-smack.controller('loginController', function ($scope, userFactory, $location, $cookies) {
+smack.controller('loginController', function ($scope, userFactory, $location, $cookies,$routeParams) {
     console.log("reached controller");
 
     $scope.user = {};
 
 
+    var currentTeamURL = $routeParams.teamURL;
+
     var errorHandler = function (errors) {
         $scope.validationErrors = errors;
         console.log(errors);
     }
+
     function setCurrentUser(currentUser) {
-        $cookies.put('currentUser', JSON.stringify(currentUser));
-        $location.path("/" );
+        $cookies.put('currentUserId', currentUser._id);
+        $location.path("/" + currentTeamURL + "/success");
 
     }
+
     $scope.getCurrentUser = function () {
         console.log("reached scope.index");
-        loginFactory.getCurrentUser(setCurrentUser);
+        userFactory.getCurrentUser(setCurrentUser);
     }
 
     $scope.login = function () {
         $scope.submitted = true;
         if ($scope.loginForm.$valid) {
             console.log($scope.user);
-            loginFactory.login($scope.user, setCurrentUser, errorHandler);
+            loginFactory.login($scope.user, currentTeamURL, setCurrentUser, errorHandler);
         } else {
             console.log("didnt pass ng validations ")
         }
