ae5e6e06a92db66021d45911d7a2a61bea4f0227
cosmicbuffalo <cosmic.buffalo.dev@gmail.com>
2018-01-08 23:55:33 -0600

    modified server file for test of mongodb connection

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express  = require( 'express' ),
     bp       = require('body-parser'),
+    mongodb  = require('mongodb'),
     path     = require( 'path' ),
     root     = __dirname,
     port     = process.env.PORT || 8000,
     app      = express();
+
+var mongoObjID = mongodb.ObjectID
 
 app.use(express.static( path.join( root, './client' )));
 app.use(express.static( path.join( root, './bower_components')));
@@ -12,7 +15,7 @@ app.use(express.static(path.join(__dirname, "uploads")));
 app.use(bp.json())
 app.use(bp.urlencoded({extended:true}))
 
-
+var db;
 
 //run schema to prep database
 require('./server/config/mongoose')
@@ -32,6 +35,23 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen( port, function() {
-  console.log( `server running on port ${ port }` );
+// app.listen( port, function() {
+//   console.log( `server running on port ${ port }` );
+// });
+
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen( port, function () {
+    console.log("App now running on port", port);
+  });
 });
